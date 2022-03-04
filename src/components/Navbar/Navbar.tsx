080df45e8e735e72a742e8ed31c836ec0ccb0d58
Adriel Gama <adrielgama@gmail.com>
2022-03-04 00:02:31 -0300

    Header styled and add modal section btn

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,45 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { Header, LogoHeader, BtnSimulation } from "./styles";
+import { Modal } from "@mui/material";
+import {
+  Header,
+  LogoHeader,
+  BtnSimulation,
+  NavLink,
+  ContainerNavLink,
+} from "./styles";
 import Logo from "../../assets/images/logo.png";
+import ContactSimulation from "../ContactSimulation/ContactSimulation";
 
 const Navbar = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <Header>
-      <LogoHeader src={Logo} alt="Logo" />
-      <BtnSimulation>Simule aqui</BtnSimulation>
+      <a href="#">
+        <LogoHeader src={Logo} alt="Logo" />
+      </a>
+      <ContainerNavLink>
+        <ContainerNavLink>
+          <NavLink href="#about">SOBRE NÓS</NavLink>
+          <NavLink href="#clients">CLIENTES</NavLink>
+          <NavLink href="#product">PRODUTO</NavLink>
+          <NavLink href="#team">EQUIPE</NavLink>
+          <NavLink href="#contact">CONTATO</NavLink>
+        </ContainerNavLink>
+        <BtnSimulation onClick={handleOpen}>Simule aqui</BtnSimulation>
+      </ContainerNavLink>
+      <Modal
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-simulation"
+        aria-describedby="modal-simulation"
+      >
+        <ContactSimulation />
+      </Modal>
     </Header>
   );
 };
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -4,12 +4,14 @@ import { Color, Theme } from "../../types/types";
 export const Header = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
   flex-direction: row;
 
+  width: 100%;
+  height: 100px;
+  position: fixed;
   padding: 0 1rem;
 
-  height: 100px;
   background-color: ${Color.light_bg};
   font-family: ${Theme.typography.fontFamily.Roboto};
   font-size: ${Theme.typography.heading.paragraph};
@@ -18,12 +20,14 @@ export const Header = styled.div`
 `;
 
 export const LogoHeader = styled.img`
-  /* width: 100%; */
+  max-width: 340px;
+  cursor: pointer;
 `;
 
 export const BtnSimulation = styled.button`
   width: 200px;
   height: 50px;
+  margin-left: 8px;
 
   background-color: ${Color.orange};
   border-radius: ${Theme.radius.default};
@@ -41,7 +45,53 @@ export const BtnSimulation = styled.button`
     background-color: ${Color.orangeHover};
   }
 
+  :focus,
+  :hover {
+    box-shadow: 0 0.5em 0.5em -0.4em ${Color.orangeHover};
+    transform: translateY(-0.25em);
+  }
   :active {
     background-color: ${Color.grey};
+  }
+`;
+
+export const ContainerNavLink = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const NavLink = styled.a`
+  text-decoration: none;
+  display: inline-block;
+  position: relative;
+
+  color: ${Color.grey};
+
+  margin: 0 1rem;
+  transition: 200ms;
+  cursor: pointer;
+
+  :after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 1px;
+    bottom: 0;
+    left: 0;
+    background-color: ${Color.orange};
+    transform-origin: bottom right;
+    transition: transform 0.25s ease-out;
+  }
+
+  :hover {
+    color: ${Color.orange};
+  }
+
+  :hover:after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
   }
 `;
