ee68a629728dd9c3e9b84d0283ff4089d145fdde
Adriel Gama <adrielgama@gmail.com>
2024-06-05 15:35:43 -0300

    fix: reorder vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Analytics />
     <App />
+    <Analytics />
   </React.StrictMode>
 )
